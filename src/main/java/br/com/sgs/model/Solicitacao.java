76b334f0342385a465b45e125518899842adc72a
Marcelo <marcelostos.dev@gmail.com>
2026-05-06 23:24:25 -0300

    feat: Criação das models Categoria, Solicitante e Solicitacao

--- src/main/java/br/com/sgs/model/Solicitacao.java
+++ src/main/java/br/com/sgs/model/Solicitacao.java
@@ -1,4 +1,103 @@
 package br.com.sgs.model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
 public class Solicitacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "solicitante_id", nullable = false)
+    private Solicitante solicitante;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "categoria_id", nullable = false)
+    private Categoria categoria;
+
+    @Column(nullable = false)
+    private String descricao;
+
+    @Column(nullable = false, precision = 19, scale = 2)
+    private BigDecimal valor;     // O tipo BigDecimal, para valores monetários, é melhor que Double ou Float devido ao erro de arredondamento
+
+    @Column(name = "data_solicitacao", nullable = false)
+    private LocalDateTime dataSolicitacao;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private StatusSolicitacao status;
+
+    public Solicitacao() {
+    }
+
+    public Solicitacao(Solicitante solicitante, Categoria categoria, String descricao, BigDecimal valor, LocalDateTime dataSolicitacao, StatusSolicitacao status) {
+        this.solicitante = solicitante;
+        this.categoria = categoria;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.dataSolicitacao = dataSolicitacao;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Solicitante getSolicitante() {
+        return solicitante;
+    }
+
+    public void setSolicitante(Solicitante solicitante) {
+        this.solicitante = solicitante;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public LocalDateTime getDataSolicitacao() {
+        return dataSolicitacao;
+    }
+
+    public void setDataSolicitacao(LocalDateTime dataSolicitacao) {
+        this.dataSolicitacao = dataSolicitacao;
+    }
+
+    public StatusSolicitacao getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusSolicitacao status) {
+        this.status = status;
+    }
 }
--- src/main/java/br/com/sgs/model/Solicitante.java
+++ src/main/java/br/com/sgs/model/Solicitante.java
@@ -1,4 +1,48 @@
 package br.com.sgs.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Solicitante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(unique = true, nullable = false)
+    private String cpfCnpj;
+
+    public Solicitante() {}
+
+    public Solicitante(String nome, String cpfCnpj) {
+        this.nome = nome;
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
 }
